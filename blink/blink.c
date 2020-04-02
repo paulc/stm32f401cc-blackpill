c8e15848dfa65b876a6ec7dcc6e01ddd1de4bf68
Paul Chakravarti <212168+paulchakravarti@users.noreply.github.com>
2020-04-02 23:32:26 +0100

    Retab

--- blink/blink.c
+++ blink/blink.c
@@ -23,26 +23,26 @@
 
 static void gpio_setup(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOB);
-	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 }
 
 int main(void)
 {
-	int i;
+    int i;
 
-	gpio_setup();
+    gpio_setup();
     gpio_toggle(GPIOC, GPIO13);
 
-	while (1) {
-		gpio_toggle(GPIOB, GPIO9);	/* LED on/off */
-        gpio_toggle(GPIOC, GPIO13);	/* LED on/off */
-		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
-			__asm__("nop");
-		}
-	}
+    while (1) {
+        gpio_toggle(GPIOB, GPIO9);  /* LED on/off */
+        gpio_toggle(GPIOC, GPIO13); /* LED on/off */
+        for (i = 0; i < 1000000; i++) { /* Wait a bit. */
+            __asm__("nop");
+        }
+    }
 
-	return 0;
+    return 0;
 }
